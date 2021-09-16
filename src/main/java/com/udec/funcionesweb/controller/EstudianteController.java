98a1dad964468733c61e41b5246cdb033955c13a
Camilo Preciado <camilopreciadorojas@gmail.com>
2021-09-16 07:32:05 -0500

    Primera version

---
+++ src/main/java/com/udec/funcionesweb/controller/EstudianteController.java
@@ -0,0 +1,33 @@
+package com.udec.funcionesweb.controller;
+
+import com.udec.funcionesweb.Dto.EstudianteDto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Camilo Preciado
+ */
+@Stateless
+@Path("/estudiantes")
+public class EstudianteController {
+    
+    
+    
+}
