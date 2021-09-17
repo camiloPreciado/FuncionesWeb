64d26dedc9a75240b7fc77c9d146ec88e72c549e
Camilo Preciado <camilopreciadorojas@gmail.com>
2021-09-16 20:11:51 -0500

    funcion de agregar y leer informacion fichero

--- src/main/java/com/udec/funcionesweb/controller/EstudianteController.java
+++ src/main/java/com/udec/funcionesweb/controller/EstudianteController.java
@@ -1,6 +1,12 @@
 package com.udec.funcionesweb.controller;
 
 import com.udec.funcionesweb.Dto.EstudianteDto;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -29,5 +35,54 @@ import javax.ws.rs.core.MediaType;
 public class EstudianteController {
     
     
+    @POST
+    @Path("/insertarPrueba")
+    @Consumes(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON) 
+    public void insertarPrueba(EstudianteDto estudaiante){
+     /*   System.out.println("Registrado correctamente " + estudaiante.getNombre());
+        System.out.println("Registrado correctamente " + estudaiante.getApellido());
+        System.out.println("Registrado correctamente " + estudaiante.getCedula());
+        System.out.println("Registrado correctamente " + estudaiante.getCorreo());
+        System.out.println("Registrado correctamente " + estudaiante.getEdad());
+      */  FileOutputStream fichero = null;
+        try {
+            fichero = new FileOutputStream("datos.txt");
+            ObjectOutputStream conexionF = new ObjectOutputStream(fichero);
+            conexionF.writeObject(estudaiante);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();            
+        }catch  (IOException ex){
+            ex.printStackTrace();
+        }finally{
+            try {
+                fichero.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }   
+        //return listaEstudiantes;
+    }
+    
+     @GET
+    @Path("/obtener")
+    @Produces(MediaType.APPLICATION_JSON)
+    public EstudianteDto obtener(){
+        FileInputStream ficheroL = null;
+        EstudianteDto e = null;      
+         try {
+             ficheroL = new FileInputStream("datos.txt");
+             ObjectInputStream conexionF = new ObjectInputStream(ficheroL);
+             e = (EstudianteDto)conexionF.readObject();
+             
+         } catch (FileNotFoundException ex) {
+             ex.printStackTrace();
+         }catch  (IOException ex){
+            ex.printStackTrace();
+        }catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        return e;
+    }   
     
 }
